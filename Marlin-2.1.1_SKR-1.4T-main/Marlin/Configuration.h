977f3665756cfaab09248d6202f60c43ab6e32fa
boriska0353 <borissor7@gmail.com>
2025-03-08 12:53:15 +0300

    Progress saved

--- Marlin-2.1.1_SKR-1.4T-main/Marlin/Configuration.h
+++ Marlin-2.1.1_SKR-1.4T-main/Marlin/Configuration.h
@@ -62,7 +62,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(thisiskeithb, Sidewinder X1)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Очумелые ручки Inc." // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -89,7 +89,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_SKR_V1_4_TURBO
+  #define MOTHERBOARD BOARD_BTT_SKR_V1_4
 #endif
 
 /**
